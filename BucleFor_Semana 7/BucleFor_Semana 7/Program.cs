b1c3f8defe400bff3f459632bb6e6de22146c592
Saul1500 <125324281+Saul1500@users.noreply.github.com>
2023-03-07 19:31:55 -0600

    Add files via upload

---
+++ BucleFor_Semana 7/BucleFor_Semana 7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class For
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Introduce un número entero: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= numero; i++)
+        {
+            for (int f = 1; f <= i; f++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
